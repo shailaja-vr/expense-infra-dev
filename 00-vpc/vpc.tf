d381c512bfc31d7c35103a7e4dc504a1e122d45c
shailajae <shailaja00145@gmail.com>
2025-07-22 11:30:08 +0530

    today error

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -13,7 +13,7 @@ module "vpc" {
 }
 
 # this can be included in module
-resource "aws_db_subnet_group" "default" {
+resource "aws_db_subnet_group" "expense" {
   name       = "${var.project_name}-${var.environment}"
   subnet_ids = module.vpc.database_subnet_ids
 
